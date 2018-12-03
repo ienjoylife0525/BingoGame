5b78dcd9d3bbf192ade6a1e7d564420494a619d3
Patato <ienjoylife0525@gmail.com>
2018-12-04 02:19:01 +0800

    Borde UI & Status Label UI

--- BingoGame/GamePage/BGGamePageViewController.swift
+++ BingoGame/GamePage/BGGamePageViewController.swift
@@ -20,6 +20,8 @@ class BGGamePageViewController: UIViewController {
         self.view.backgroundColor = UIColor.white
         self.title = "Game Page"
         settingBtnSet()
+        borderViewSet()
+        statusLabelSet()
     }
     
     private func settingBtnSet() {
@@ -27,7 +29,16 @@ class BGGamePageViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = settingBtn
     }
     
+    private func statusLabelSet() {
+        let m_lbStatus = UILabel(frame: CGRect(x: 10, y: 100, width: self.view.frame.width - 30, height: 40))
+        m_lbStatus.text = "Set the game first !!"
+        m_lbStatus.textColor = UIColor.darkGray
+        self.view.addSubview(m_lbStatus)
+    }
     private func borderViewSet() {
+        let m_vBorder = UIView(frame: CGRect(x: 10, y: 180, width: self.view.frame.width - 20, height: self.view.frame.width - 20))
+        m_vBorder.backgroundColor = UIColor.gray
+        self.view.addSubview(m_vBorder)
         
     }
     
